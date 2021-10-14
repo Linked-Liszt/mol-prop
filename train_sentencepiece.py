54f1ecc05d74a6a2408445b664319994e5bf98d9
Oxymoren <18706708+Oxymoren@users.noreply.github.com>
2021-10-14 18:08:55 -0500

    Working training

--- train_sentencepiece.py
+++ train_sentencepiece.py
@@ -4,7 +4,7 @@
 
 import sentencepiece as spm
 spm.SentencePieceTrainer.train(input='data/proc_zinc/all.txt',
-                               input_sentence_size=5000000,
+                               #input_sentence_size=7000000,
                                shuffle_input_sentence=True,
-                               vocab_size=1000,
-                               model_prefix='smiles_sp')
+                               vocab_size=455,
+                               model_prefix='models/smiles_sp')
