e404fb1178a9ca596d07a2ed40de0004025ec0db
Oxymoren <18706708+Oxymoren@users.noreply.github.com>
2021-10-15 22:55:46 -0500

    Add fine-tuning script

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 from typing import List
 import re
 import csv
+from tqdm import tqdm
+import os
 
 SMI_REGEX_PATTERN = r"(\[[^\]]+]|Br?|Cl?|N|O|S|P|F|I|b|c|n|o|s|p|\(|\)|\.|=|\n#|-|\+|\\|\/|:|~|@|\?|>>?|\*|\$|\%[0-9]{2}|[0-9])"
 
@@ -24,3 +26,20 @@ def import_smiles(data_fp: str, skiprow: bool = False, include_y: bool = False):
         return smiles_tokens, ys
     else:
         return smiles_tokens
+
+def compute_max_length(dataset, data_key: str, tokenizer, max_len_path: str) -> int:
+    if os.path.exists(max_len_path):
+        max_len = int(open(max_len_path).read())
+    else:
+        max_len = 0
+        with tqdm(total=len(dataset['train'])) as pbar:
+            for data in dataset['train']:
+                max_len = max(len(tokenizer(data[data_key])['input_ids']), max_len)
+                pbar.update(1)
+        with open(max_len_path, 'w') as f:
+            f.write(str(max_len))
+
+    # Nessesary for the data collator
+    if max_len % 2 == 1:
+        max_len += 1
+    return max_len
